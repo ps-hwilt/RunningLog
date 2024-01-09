0a1b8d5f0741319630ab8e82853a95e585870127
ps-hwilt <henry.wilt@crisphealth.org>
2024-01-09 12:09:33 -0500

    Basic Data + Model set up

---
+++ src/RunningLog/Models/Activity.cs
@@ -0,0 +1,13 @@
+namespace RunningLog.Models;
+
+public class Activity
+{
+    public int Id { get; set; }
+    
+    public string Time { get; set; }
+    
+    public string Distance { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+}
---
+++ src/RunningLog/Repositories/InMemActivitiesRepository.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http.Features;
+using RunningLog.Models;
+
+namespace RunningLog.Repositories;
+
+public class InMemActivitiesRepository
+{
+    private readonly List<Activity> _activities = new()
+    {
+        new Activity
+        {
+            Id = 1,
+            Distance = "5",
+            Time = "00:40:23",
+            StartTime = DateTime.UtcNow
+        },
+        new Activity
+        {
+            Id = 2,
+            Distance = "3",
+            Time = "00:23:00",
+            StartTime = DateTime.UtcNow
+        },
+        new Activity
+        {
+            Id = 3,
+            Distance = "1",
+            Time = "00:4:35",
+            StartTime = DateTime.UtcNow
+        }
+    };
+
+    public IEnumerable<Activity> GetActivities()
+    {
+        return _activities;
+    }
+
+    public Activity GetActivity(int Id)
+    {
+        return _activities.SingleOrDefault(a => a.Id == Id);
+    }
+}
